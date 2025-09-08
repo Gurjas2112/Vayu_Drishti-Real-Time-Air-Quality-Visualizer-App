42e15e151b2f7979f889a69e4dbe981db6a24bc7
Gurjas Gandhi <115103812+Gurjas2112@users.noreply.github.com>
2025-09-08 16:24:23 +0530

    added the frontend code

--- frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.vayu_drishti
+package com.example.frontend
 
 import io.flutter.embedding.android.FlutterActivity
 
--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -1,13 +1,30 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vayu_drishti/main.dart';
+import 'package:frontend/main.dart';
 
 void main() {
-  testWidgets('VayuDrishti app test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const VayuDrishtiApp());
+    await tester.pumpWidget(const MyApp());
 
-    // Verify that our app starts with splash screen.
-    expect(find.text('VayuDrishti'), findsOneWidget);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
